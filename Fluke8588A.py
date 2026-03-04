56878f3da4a08053f5fa4fa41b849a62233b2291
Isar <isar@kavlie.no>
2026-03-04 12:54:35 +0100

    feat(LIB): now doesnt handle exceptions and wrong values, simlpy returns acutally set value. Up to the software that interacts with the library to decide what to do

--- Fluke8588A.py
+++ Fluke8588A.py
@@ -19,9 +19,7 @@ class Fluke8588A(object):
 
 	def __init__(self, address):
 		logging.info(__name__ + ' : Initializing instrument Fluke 8588A')
-		rm = pyvisa.ResourceManager()
-		self._address = address
-		self._instr = rm.open_resource("GPIB0::" + str(self._address) + "::INSTR")
+		self.__connect(address)
 		self._instr.timeout = 10e3
 		self.plc_max = 500
 
@@ -34,6 +32,12 @@ class Fluke8588A(object):
 		# self.write(":SENSE:VOLT:DC")
 		# self.write(":SENSE:VOLT:DC:RANG:AUTO ON")        
 		print("\nInstrument %s successfully initialized. \n" % idn_string)
+
+	def __connect(self, address): #private to be only used by init
+		rm = pyvisa.ResourceManager()
+		self._address = address
+		self._instr = rm.open_resource("GPIB0::" + str(self._address) + "::INSTR")
+		
 	'''returns *IDN?'''
 	def identify(self):
 		return self._instr.query("*IDN?")
@@ -86,67 +90,95 @@ class Fluke8588A(object):
 	def close(self):
 		self._instr.close()
 
-	def set_plc(self, plc):
-		'''
-		Change NPLC value
-		'''
-		if plc > self.plc_max: plc = self.plc_max
-		self._instr.write(":SENSE:VOLT:DC:NPLC %.3f" % plc) 
- 
 	def init_dcv(self, range, resolution, zin, measure_mode, nplc):
 		'''
 		Set the machine to dcv mode, and set up parameters
 		'''
-		root_commands=":VOLT:DC"
+		root=":VOLT:DC"
 		initStatus=[]
 		self.write(":FUNC \":VOLT:DC\"")
-		initStatus.append(self.setRange(root_commands, range))
-		initStatus.append(self.setResolution(root_commands, resolution))
-		initStatus.append(self.setImpedence(root_commands, zin))
-		initStatus.append(self.setNplc(root_commands, measure_mode, nplc))
-	
+		initStatus.append(self.setRange(root, range))
+		initStatus.append(self.setResolution(root, resolution))
+		initStatus.append(self.setImpedence(root, zin))
+		initStatus.append(self.setNplc(root, measure_mode, nplc))
+
 	def getNplc(self, root):
+		'''
+		Get Nplc value for given root
+		Input:
+			root
+		Output:
+			set value
+		'''
 		return self.query(root+":NPLC?")
 	def setNplc(self, root, value):
-		try:
-			self.write(root+":NPLC "+str((value)))
-		except Exception as e:
-			print(f"plc variable out of range {e}")
-			return -1
-		return self.query(root+":NPLC?")
+		'''
+		Set NPLC value for given root
+		Input:
+			root, desired value
+		Output:
+			set value
+		'''
+		self.write(root+":NPLC "+str((value)))
+		return self.getNplc
 
 	def getImp(self, root):
+		'''
+		Get Impedence value for given root
+		Input:
+			root
+		Output:
+			set value
+		'''
 		return self.query(root+":IMP?")
 	def setImpedence(self, root, value):
-		try:
-			if not value in sbv.getDcvZin():
-				raise ValueError("impedance has to be" + str(sbv.getDcvZin()))
-		except (ValueError, TypeError):
-			print("Impedance value error")
-			return self.getImp(root)
+		'''
+		Set Impedence value for given root
+		Input:
+			root, desired value
+		Output:
+			set value
+		'''
 		self.write(root+":IMP "+value)
 		return self.getImp(root)
 	
 	def getRange(self, root):
-		"""returns set range"""
+		'''
+		Get range value for given root
+		Input:
+			root
+		Output:
+			set value
+		'''
 		return int(self.query(root+":RANG?"))
 	def setRange(self, root, value):
-		"""sets given range, return actual set range"""
-		try:
-			if (root == "DCV" and value not in sbv.getDcvRange()) or (root == "DCI" and value not in sbv.getDciRange()):				
-				raise ValueError("range not in " + root +" range List")
-		except:
-			return self.getRange(root)
-		self.write(root+":RANG: "+str(sbv.getDcvRangeVal(value)))
+		'''
+		Set range value for given root
+		Input:
+			root, desired value
+		Output:
+			set value
+		'''
+		self.write(root+":RANG: "+value)
 		return self.getRange(root)
 	
 	def getResolution(self, root):
+		'''
+		Get resolution value for given root
+		Input:
+			root
+		Output:
+			set value
+		'''
 		return int(self.query(root+":RES?"))
+	
 	def setResolution(self, root, value):
-		try:
-			if not value in sbv.getDcDigitVal(): #doesnt work with AC right now
-				raise ValueError("resolution has to be " + str(sbv.getDcResolution()))
-		except ValueError:
-			return self.getResolution(root)
+		'''
+		Get resolution value for given root
+		Input:
+			root
+		Output:
+			set value
+		'''
 		self.write(root+":RES "+value)
 		return self.getResolution(root)
