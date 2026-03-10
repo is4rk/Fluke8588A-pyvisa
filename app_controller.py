95aa1691f51d22f87372f274325c7ad758c839f3
Isar <isar@kavlie.no>
2026-03-10 16:51:58 +0100

    feat(View, Presenter): first test version of OOP method

---
+++ app_controller.py
@@ -0,0 +1,24 @@
+from Fluke8588A import Fluke8588A
+from main_window import MainWindow
+
+class AppController:
+
+	def __init__(self):
+		self._view = MainWindow()
+		self._instr_ctrl = Fluke8588A(9) #for now hard coded, should be called after set_init 
+		self._connect_signals()
+		self._view.show()
+
+	def _connect_signals(self):
+		self._view.read_requested.connect(self._on_read)
+		self._meas_ctrl.reading_ready.connect(self._view.set_reading)
+
+	def _on_read(self):
+		if not self._instr_ctrl.is_connected():
+			self._view.set_status("ERROR: not connected.")
+			return
+		try:
+			self._meas_ctrl.read()
+		except Exception as e:
+			self._view.set_status(f"Read error: {e}")
+
---
+++ main_window.py
@@ -0,0 +1,65 @@
+from PyQt6 import uic
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QMainWindow, QWidget
+import os
+main_window_loc = os.path.join(os.path.dirname(__file__), "ui", "mainwindow.ui")
+
+class MainWindow(QMainWindow):
+	
+	init_requested = pyqtSignal()
+	mode_changed = pyqtSignal(str)
+	read_requested = pyqtSignal()
+	set_requested = pyqtSignal()
+	measurment_setup_requested = pyqtSignal()
+
+	def __init__(self):
+		super().__init__()
+		uic.loadUi(main_window_loc, self)
+		self._init_always_visible()
+	#	self._init_mode_widgets()
+		self._connect_signals()
+
+	def _init_always_visible(self):
+		self.init_button.setVisible(True)
+		for widget in self.findChildren(QWidget):
+			widget.setVisible(False)
+		self.init_button.setVisible(True)
+		self.mode_combo.setVisible(True)
+		self.read_button.setVisible(True)
+		self.set_button.setVisible(True)
+		self.status_label.setVisible(True)
+			
+	def _connect_signals(self):
+		#always visible
+		self.init_button.pressed.connect(self.init_requested)
+		self.mode_combo.currentTextChanged.connect(self.mode_changed)
+		self.read_button.pressed.connect(self.read_requested)
+		self.set_button.pressed.connect(self.set_requested)
+		self.dcv_measure_setup_button.pressed.connect(self.measurment_setup_requested)
+
+	@property
+	def current_gpib_address(self)->int:
+		return self.gpib_addr_spin.value()
+
+	@property
+	def current_dcv_range(self)->str:
+		return self.dcv_range_combo.currentText()
+
+	@property
+	def current_dcv_resolution(self)->int:
+		return self.dcv_res_spin.value()
+
+	@property
+	def current_dcv_measurement_mode(self)->str:
+		return self.dcv_measure_setup_button.currentText()
+
+	@property
+	def current_dcv_zin(self)->str:
+		return self.dcv_zin_combo.currentText()
+
+		
+	def set_mode_visible(self, mode: str):
+		self.dcv_widget.setVisible(mode == "DCV")
+		self.dci_widget.setVisible(mode == "DCI")
+		self.acv_widget.setVisible(mode == "ACV")
+		self.aci_widget.setVisible(mode == "ACI")
