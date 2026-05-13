1c83362b03c7b48396ee5ed6db6ef23e85025f9f
Isar <isar@kavlie.no>
2026-05-13 14:14:35 +0200

    fix(test): FUNC?

--- test_resistance.py
+++ test_resistance.py
@@ -32,7 +32,7 @@ def test_resistance():
         # Take measurement
         reading = fluke.read()
         print(f"Resistance: {reading}")
-        funct=":FUNC \"" + ":CURR:DC"+ "\""
+        fluke.write(":FUNC \":CURR:DC\"")
         funct= fluke.query(":FUNC?")
         print(funct)
         fluke.close()
